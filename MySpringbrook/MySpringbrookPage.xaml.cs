563a8c17cb1afa6653b82d04172d0672a3916777
THE-NEW-EPOCH <2e9058f38091189e1cf183acae50ef90d37b822f>
2016-11-08 11:32:50 -0500

    Initial check-in of module MySpringbrook

---
+++ MySpringbrook/MySpringbrookPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MySpringbrook
+{
+	public partial class MySpringbrookPage : ContentPage
+	{
+		public MySpringbrookPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
